06457797d44d2d4c298dfa1a3f0bab33531495eb
Jessica Huynh <huynhjessica19@gmail.com>
2020-02-27 14:17:08 -0800

    adding visual affects

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#606bd3",
+        "#60c2d3"
+    ];
 
 
     //sounds
@@ -10,4 +19,11 @@ window.addEventListener('load', () => {
             sounds[index].play();
         });
     });
+
+
+    const createBubbles = () => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[]
+    };
 });
