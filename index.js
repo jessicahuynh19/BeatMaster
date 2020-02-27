57a5961eabe448d86dadfd52a8a074eaf0ab2e5c
Jessica Huynh <huynhjessica19@gmail.com>
2020-02-27 14:06:48 -0800

    minor edits

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
-    const sounds = document.querySelector
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+
+
+    //sounds
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click", function(){
+            sounds[4];
+        });
+    });
 });
+
